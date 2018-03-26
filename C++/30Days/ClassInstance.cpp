53a6b697b7fb01b34faa6b088114539305260724
Mann <patelmann1999@gmail.com>
2018-03-26 19:17:39 -0400

    Completed challenge 4 & 5

--- C++/30Days/ClassInstance.cpp
+++ C++/30Days/ClassInstance.cpp
@@ -15,31 +15,55 @@ class Person {
 }; 
 
 Person::Person(int initialAge) {
-	this.age = initialAge; 
+	if (initialAge < 0) {
+		this->age = 0; 
+		cout << "Age is not valid, setting age to 0." << endl; 
+	}
+
+	else 
+		this->age = initialAge; 
 }
 
+void Person::yearPasses() {
+	++age; 
+	
+	return; 
+}
+
+void Person::amIOld() {
+	if (this->age < 13) {
+		cout << "You are young." << endl; 
+	}
+
+	else if (this->age >= 13 && this->age < 18) {
+		cout << "You are a teenager." << endl; 
+	} 
+
+	else 
+		cout << "You are old." << endl; 
+
+	return; 
+}
 
 int main (const int argc, const char* const argv[]) {
-	return 1; 
-}
+	int t; 
+	int age; 
 
-/*
-int main(){
-    int t;
-	int age;
-    cin >> t;
-    for(int i=0; i < t; i++) {
-    	cin >> age;
-        Person p(age);
-        p.amIOld();
-        for(int j=0; j < 3; j++) {
-        	p.yearPasses(); 
-        }
-        p.amIOld();
-      
-		cout << '\n';
-    }
+	cin >> t; 
 
-    return 0;
+	for (int i = 0; i < t; i++) {
+		cin >> age; 
+		//created an object of the person class
+		Person p(age); 
+
+		for (int j = 0; j < 3; j++) { 
+			p.yearPasses(); 
+		}
+
+		p.amIOld(); 
+
+		cout << '\n'; 
+	} 
+
+	return 0; 
 }
-*/
