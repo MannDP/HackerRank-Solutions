15262874a831b900d1014e9168e921406f64e10e
Mann <patelmann1999@gmail.com>
2018-03-26 22:18:59 -0400

    all logic implemented in main

--- C++/30Days/Review.cpp
+++ C++/30Days/Review.cpp
@@ -8,7 +8,51 @@ using namespace std;
 
 int main() {
     
-    
+    int t; 
+    cin >> t; 
+
+    for (int i = 0; i < t; i++) {
+    	string input; 
+    	cin >> input; 
+
+    	int count = 1; 
+    	for (int i = 0; input[i] != 0; i++) {
+    		count++; 
+    	}
+
+    	count = count - 1; 
+
+    	int even_count; 
+    	int odd_count; 
+
+    	if (count % 2 == 0) {
+    		even_count = count/2; 
+    		odd_count = count/2; 
+    	}
+
+    	else if (count % 2 != 0) {
+    		odd_count = (count - 1)/2; 
+    		even_count = count - odd_count; 
+    	}
+
+    	cout << "count is: " << count << " even count is: " << even_count << " odd count is: " << odd_count << endl; 
+
+    	//need to print out all the even numbers first
+    	int index_counter = 0; 
+    	for (int m = 0; index_counter <= even_count; m+= 2) {
+    		cout << input[m]; 
+    		++index_counter; 
+    	}
+
+    	cout << " "; 
+
+    	index_counter = 1; 
+    	for (int m = 1; index_counter <= odd_count; m+= 2) {
+    		cout << input[m]; 
+    		++index_counter; 
+    	}
+    }
+
 
     return 0;
 }
